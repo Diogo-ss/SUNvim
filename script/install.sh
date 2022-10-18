3097417fd55adfdbe75336a9180cb65c4c479caf
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-18 01:09:13 -0300

    add install

---
+++ script/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
+
+cd /home/$USER/.config/
+git clone https://github.com/Diogo-ss/SUNvim.git
+
+mv /home/$USER/.config/SUNvim /home/$USER/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim -c 'PlugInstall'
