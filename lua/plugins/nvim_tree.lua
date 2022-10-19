3cde61ce97ebad47b8b1490709f7147b4dd5c324
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-19 11:38:01 -0300

    fix ntree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -17,8 +17,7 @@ require("nvim-tree").setup({
     	},
 	},
 	diagnostics = {
-    	enable = true,
-		enable = false,
+		enable = true,
         show_on_dirs = false,
         debounce_delay = 50,
         icons = {
