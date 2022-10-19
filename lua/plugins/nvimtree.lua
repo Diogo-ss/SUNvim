ab3b86e17dbc4d0ddd9c9a033314adb61ced85fa
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-18 22:33:32 -0300

    nvim tree icons and lsp

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -34,5 +34,43 @@ require("nvim-tree").setup({
             	none = " ",
 			},
 		},
+		icons = {
+			webdev_colors = true,
+			git_placement = "before",
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				bookmark = "",
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
 	},
+
+
 })
