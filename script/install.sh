4d2b3f64b004c670025d8f4a07a2f6fab66c1fb7
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-19 16:49:44 -0300

    add --depth 1 in sh

--- script/install.sh
+++ script/install.sh
@@ -5,7 +5,7 @@ rm -rf ~/.local/share/nvim
 rm -rf ~/.cache/nvim
 
 cd /home/$USER/.config/
-git clone https://github.com/Diogo-ss/SUNvim.git
+git clone https://github.com/Diogo-ss/SUNvim.git --depth 1
 
 mv /home/$USER/.config/SUNvim /home/$USER/.config/nvim
 
