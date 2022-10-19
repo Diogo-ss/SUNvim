a8a8651412ed3bfef5bd5dd39897bb8ac0f5a8ad
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-18 22:20:45 -0300

    fix formating

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -2,20 +2,20 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    hide_root_folder = true,
-    side = "left",
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
+	sort_by = "case_sensitive",
+	view = {
+    	hide_root_folder = true,
+    	side = "left",
+    	number = false,
+    	relativenumber = false,
+    	signcolumn = "yes",
+    	adaptive_size = true,
+    	mappings = {
+      		list = {
+        	{ key = "u", action = "dir_up" },
+      		},
+    	},
+	},
 	diagnostics = {
     	enable = true,
   	},
