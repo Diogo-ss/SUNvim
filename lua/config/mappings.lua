0a21b3c64b5ced21a9af595769c5bc883ce8c00a
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-10-19 16:22:29 -0300

    fix exit update

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -111,6 +111,6 @@ map('n', '<leader>i', '<CMD>ToggleTerm size=10 direction=float<CR>')						-- sel
 
 -- ranger
 map('n', '<leader>rr', '<CMD>TermExec size=10 direction=float cmd=ranger<CR>')										-- abre o Ranger no diretório atual
-map('n', '<leader>uu', "<CMD>TermExec size=10 direction=float cmd='sh ~/.config/nvim/script/update.sh && sleep 2'<CR>") 	-- SUNvim update
+map('n', '<leader>uu', "<CMD>TermExec size=10 direction=float cmd='sh ~/.config/nvim/script/update.sh && exit'<CR>") 	-- SUNvim update
 map('n', '<leader>uf', "<CMD>TermExec size=10 direction=float cmd='sh ~/.config/nvim/script/fix_list.sh'<CR>") 	-- SUNvim update
 
